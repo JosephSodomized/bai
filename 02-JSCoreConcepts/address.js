148409362b37569d9427ac0e2ccd02a723a2667a
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-03-16 00:44:14 +0100

    first 2 done

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -6,8 +6,13 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
+let first = title + ' ' + name + ' ' + surname + '\n' + 'ul. ' + street + '\n' + zip + ' ' + city + '\n' + country.toUpperCase();
+let second = `${title} ${name} ${surname}\nul. ${street}\n${zip} ${city}\n${country.toUpperCase()}`;
+console.log(first);
+console.log(second);
+// console.log('dr Mateusz Kubicki \nul. Długa 15\n30-781 Kraków\nPOLAND'); --> nie wiem, czy również o taki zapis chodziło
 
-/* 
+/*
 dr Mateusz Kubicki
 ul. Długa 15
 30-781 Kraków
--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -6,6 +6,10 @@ h = 7
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+let a = () => {
+    let p = 1/2*(b+c+h);
+    let pole = Math.sqrt(p*((p-b)*(p-c)*(p-h)));
+    return pole;
+};
 
-console.log( a() );
+console.log(a());
