891fd2390ad59d58f36cef0bcedbdda97f34f953
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-03-19 00:12:48 +0100

    fixed, my bad...

--- 02-JSCoreConcepts/person.js
+++ 02-JSCoreConcepts/person.js
@@ -1,13 +1,20 @@
-class Person{
-    constructor(name, surname){
+class Person {
+    constructor(name, surname) {
         this.name = name;
         this.surname = surname;
     }
+    getName() {
+        return this.name + ' ' + this.surname.toUpperCase();
+    }
 
-    giveName(){
-        return name+' '+surname.toUpperCase();
+    getInicials() {
+        return this.name[0].toUpperCase() + '.' + this.surname[0].toUpperCase() + '.';
     }
 }
 
-var jack = new Person("Dominik", "Malcharczyk");
-console.log(jack.giveName());
+var jn = new Person("Jan", "Nowak");
+var dm = new Person("Dominik", "Malcharczyk");
+console.log(jn.getName());
+console.log(jn.getInicials());
+console.log(dm.getName());
+console.log(dm.getInicials());
