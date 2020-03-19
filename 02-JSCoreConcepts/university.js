aab1d467be0bbeff71018832d791a0d0c286ac97
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-03-19 16:32:57 +0100

    problem with 2 functions -> to do

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -2,33 +2,45 @@ names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
-    startWith(letter){
-        return // names (array) which starts with the letter
+    startWith(letter) {
+        var arr = [];
+        for (let i = 0; i < names.length; i++) {
+            if (names[i][0] == letter) {
+                arr.push(names[i])
+            }
+        }
+        return arr;
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return this.names.sort(); // names (array) in alphabetical order 
     }
 
     get() {
-        return // names (array) in natural order
+        return names;// names (array) in natural order
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        var sorted = this.names.sort()// get first 'n' names (array) 
+        var newArr = []
+        for (let i = 0; i < n; i++) {
+            newArr.push(sorted[i])
+        }
+        return newArr;
     }
 }
 
 students = new Students(names);
 console.log(students.get());
-console.log(students.sort());
+console.log(students.get().sort());
 console.log(students.startWith('P'));
 console.log(students.startWith('M').sort());
-console.log(students.sort().getFirst(4));
-console.log(students.startWith('M').sort().getFirst(2));
+// console.log(students.sort().getFirst(4));
+console.log(students.getFirst(4));
+console.log(students.startWith('M').getFirst(2));
 console.log(students.get());
 
 
